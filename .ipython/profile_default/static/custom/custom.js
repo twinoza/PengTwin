0b70ed9e1fae22c6f3a99767fe44e747736bbcc4
noza <noza@stanford.edu>
2015-03-27 13:34:32 -0700

    Updated custom.js to Ben's latest version
    This version makes the custom.js file compatible with iPython3 and
    therefore Jupyter.

--- .ipython/profile_default/static/custom/custom.js
+++ .ipython/profile_default/static/custom/custom.js
@@ -1,109 +1,149 @@
-// leave at least 2 line with only a star on it below, or doc generation fails
 /**
  *
+ * This file contains javascript extensions for IPython notebook,
+ * compatible with Jupyter. The file contains the following extensions
  *
- * Placeholder for custom user javascript
- * mainly to be overridden in profile/static/custom/custom.js
- * This will always be an empty file in IPython
+ * a. Button to launch QTConsole
+ * b. Button to show/hide all input cells
+ * c. Toolbar entry to add show/hide checkbox for each cell
+ * d. Hide all marked cells when Notebook is launched
+ * e. Open gVim to edit current cell and update with saved content
  *
- * User could add any javascript in the `profile/static/custom/custom.js` file
- * (and should create it if it does not exist).
- * It will be executed by the ipython notebook at load time.
+ * Other settings:
+ * i.  Show line numbers by default in code cells
+ * ii. Show equation number when using Mathjax
  *
- * Same thing with `profile/static/custom/custom.css` to inject custom css into the notebook.
- *
- */
-
-/**
- * Example :
- *
- *  Use `jQuery.getScript(url [, success(script, textStatus, jqXHR)] );`
- *  to load custom script into the notebook.
- *
- *    // to load the metadata ui extension example.
- *    $.getScript('/static/notebook/js/celltoolbarpresets/example.js');
- *    // or
- *    // to load the metadata ui extension to control slideshow mode / reveal js for nbconvert
- *    $.getScript('/static/notebook/js/celltoolbarpresets/slideshow.js');
- *
- *
- * @module IPython
- * @namespace IPython
- * @class customjs
- * @static
+ * Ben Varkey Benjamin : 21-Mar-2015
  */
 
 /*
- * Code to hide the raw code notebook wide
+ * Code to hide the raw code
  * ref:
  * [1] http://blog.nextgenetics.net/?e=102
  */
 code_show=true; 
 function code_toggle() {
-  if (code_show){
-    $('div.input').hide();
-  } else {
-    $('div.input').show();
-  }
-  code_show = !code_show;
+    if (code_show){
+        $('div.input').hide();
+    } else {
+        $('div.input').show();
+    }
+    code_show = !code_show;
 } 
 
-/*
- * Create a custom button to hide/show inputs, notebook wide.
- * Create a custom button in toolbar that executes `%qtconsole` in kernel
- * and hence open a qtconsole attached to the same kernel as the current notebook
- */
-$([IPython.events]).on('app_initialized.NotebookApp', function(){
-  IPython.toolbar.add_buttons_group([
-    {
-      'label'   : 'Run QTConsole',
-      'icon'    : 'icon-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
-      'callback': function () {
-        IPython.notebook.kernel.execute('%qtconsole')
-      }
-    },
-    {
-      'label'   : 'Toggle Code',
-      'icon'    : 'icon-code', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
-      'callback': function () {
-          $( document ).ready(code_toggle);
-      }
-    }
-  ]);
+require([
+    'base/js/namespace',
+    'base/js/events'
+    ], function(IPython, events) {
+        events.on("app_initialized.NotebookApp", function () {
+            IPython.toolbar.add_buttons_group([
+                /**
+                 * a. Button to launch QTConsole
+                 */
+                {
+                     'label'   : 'Run QTConsole',
+                     'icon'    : 'fa-terminal', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
+                     'callback': function () {
+                         IPython.notebook.kernel.execute('%qtconsole')
+                     }
+                },
+                /*
+                 * b. Button to show/hide all input cells
+                 */
+                {
+                     'label'   : 'Toggle Code',
+                     'icon'    : 'fa-code', // select your icon from http://fortawesome.github.io/Font-Awesome/icons
+                     'callback': function () {
+                        $( document ).ready(code_toggle);
+                     }
+                }
+                
+            ]);
+            /*
+             * c. Toolbar entry to add show/hide checkbox for each cell
+             */
+            var CellToolbar = IPython.CellToolbar;
+            var show_hide_input = CellToolbar.utils.checkbox_ui_generator('Hide Input',
+             // setter
+             function(cell, value){
+                 // we check that the _draft namespace exist and create it if needed
+                 if (cell.metadata._ben == undefined){cell.metadata._ben = {};}
+                    // set the value
+                 cell.metadata._ben.show_hide_input = value;
+                 if(value === true){
+                    cell.element.children()[0].childNodes[1].childNodes[1].style.display="none";
+                 }else if(value === false){
+                    cell.element.children()[0].childNodes[1].childNodes[1].style.display="block";
+                 }},
+             //getter
+             function(cell){ var ns = cell.metadata._ben;
+                 // if the _draft namespace does not exist return undefined
+                 // (will be interpreted as false by checkbox) otherwise
+                 // return the value
+                    return (ns == undefined)? undefined: ns.show_hide_input;
+                 }
+             );
+            CellToolbar.register_callback('show_hide_input.chkb', show_hide_input);
+            CellToolbar.register_preset('Show/Hide Input', ['show_hide_input.chkb']);
+            /* 
+             * e. Open gVim to edit current cell and update with saved content 
+             * Use 'g' to open gvim. Use 'u' after saving and exiting gvim.
+             *
+             * I think I found this code on some Stackoverflow page.
+             *
+             */
+            IPython.keyboard_manager.command_shortcuts.add_shortcut('g', {
+                handler : function (event) {
+                    var input = IPython.notebook.get_selected_cell().get_text();
+                    var cmd = "f = open('/tmp/.ipycell.py', 'w');f.close()";
+                    if (input != "") {
+                        cmd = '%%writefile /tmp/.ipycell.py\n' + input;
+                    }
+                    IPython.notebook.kernel.execute(cmd);
+                    cmd = "import os;os.system('gvim /tmp/.ipycell.py')";
+                    IPython.notebook.kernel.execute(cmd);
+                    return false;
+                }}
+            );
+            IPython.keyboard_manager.command_shortcuts.add_shortcut('u', {
+                handler : function (event) {
+                    function handle_output(msg) {
+                        var ret = msg.content.text;
+                        IPython.notebook.get_selected_cell().set_text(ret);
+                    }
+                    var callback = {'output': handle_output};
+                    var cmd = "f = open('/tmp/.ipycell.py', 'r');print(f.read())";
+                    IPython.notebook.kernel.execute(cmd, {iopub: callback}, {silent: false});
+                    return false;
+                }}
+            );
+
+            // i. Show line numbers by default
+            IPython.CodeCell.options_default.cm_config["lineNumbers"] = true;
+        });
 });
 
-/*
- * Add a toolbar entry to show/hide input on a cell-by-cell basis.
- * This could break in some future version of IPython, if they decide
- * to change the DOM structure.
- *
- * ref:
- * [1] http://nbviewer.ipython.org/github/Carreau/posts/blob/master/04-initialisation-cell.ipynb
- * [2] http://www.javascriptkit.com/javatutors/dom3.shtml
+/* 
+ * d. Hide all marked cells when Notebook is launched
+ * 
+ * Ref: http://nbviewer.ipython.org/github/Carreau/posts/blob/master/04-initialisation-cell.ipynb
  */
-
-var CellToolbar= IPython.CellToolbar;
-var show_hide_input = CellToolbar.utils.checkbox_ui_generator('Show/Hide Input',
-  // setter
-  function(cell, value){
-    // we check that the _ben namespace exist and create it if needed
-    if (cell.metadata._ben == undefined){cell.metadata._ben = {};}
-    // set the value
-    cell.metadata._ben.show_hide_input = value;
-    if(value == true){
-      cell.element.children()[0].childNodes[1].childNodes[1].style.display="none";
-    }else if(value == false){
-      cell.element.children()[0].childNodes[1].childNodes[1].style.display="block";
+var hide_init = function(){
+    var cells = IPython.notebook.get_cells();
+    for(var i in cells){
+        var cell = cells[i];
+        var namespace =  cell.metadata._ben|| {};
+        var isHidden = namespace.show_hide_input;
+        // you also need to check that cell is instance of code cell,
+        // but lets keep it short
+        if( isHidden === true){
+            cell.element.children()[0].childNodes[1].childNodes[1].style.display="none";
+        }
     }
-  },
-  //getter
-  function(cell){
-    var ns = cell.metadata._ben;
-    // if the _ben namespace does not exist return undefined
-    // (will be interpreted as false by checkbox) otherwise
-    // return the value
-    return (ns == undefined)? undefined: ns.show_hide_input;
-  }
-);
-CellToolbar.register_callback('show_hide_input.chkb', show_hide_input);
-CellToolbar.register_preset('Show/Hide Input', ['show_hide_input.chkb']);
+};
+$([IPython.events]).on('notebook_loaded.Notebook', hide_init);
+
+// ii. Numbering for IPython notebook equations
+MathJax.Hub.Config({
+      TeX: { equationNumbers: { autoNumber: "AMS" } }
+});
